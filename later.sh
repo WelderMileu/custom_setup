4063f2e26fa1aee251e2272a5ed9fb66654254b4
WelderMileu <45273258+WelderMileu@users.noreply.github.com>
2022-08-26 21:22:39 +0000

    add new file
    add file later.sh

---
+++ later.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Installing themes of ohmyzsh
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# Replace on plugins in .zshrc
+cat ~/.zshrc | sed -e s/"plugins(git)"/"plugins(git zsh-autosuggestions zsh-syntax-highlighting)"/g > ~/.zshrc-update
+
+# Add alias in .zshrc-update
+echo "alias rustscan='sudo docker run --rm --name rustscan rustscan/rustscan:1.10.0'" >> ~/.zshrc-update
+echo "alias lynxb='lynx -accept_all_cookies -update_term_title google'" >> ~/.zshrc-update
+
+# message finallity
+echo "";
+echo "romove .zshrc";
+echo "modify on name .zshrc-update of .zshrc";
+echo "source .zshrc of update";
